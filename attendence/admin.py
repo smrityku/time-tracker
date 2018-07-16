bf7d374899dff374d41e46ea2c350e05979019b7
Tasnim Mehzabin <tasnim@bdipo.com>
2018-07-16 11:28:52 +0600

    Code is refactored

--- attendence/admin.py
+++ attendence/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Attendence
+from .models import Attendance
 from .models import Leave
 from .models import LeaveTracker
 
 
-class AttendenceAdmin(admin.ModelAdmin):
+class AttendanceAdmin(admin.ModelAdmin):
 	list_display = ('user', 'check_in_date', 'in_time', 'out_time')
 
-admin.site.register(Attendence, AttendenceAdmin)
+admin.site.register(Attendance, AttendanceAdmin)
 admin.site.register(Leave)
 admin.site.register(LeaveTracker)
